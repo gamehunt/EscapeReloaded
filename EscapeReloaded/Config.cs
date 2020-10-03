249e091fdaed6550d09a4e16081aa9459df931e0
gamehunt <mt.demakov@gmail.com>
2020-10-03 13:25:01 +0300

    Removed unused imports

--- EscapeReloaded/Config.cs
+++ EscapeReloaded/Config.cs
@@ -1,5 +1,4 @@
 ﻿using Exiled.API.Interfaces;
-using System.ComponentModel;
 
 namespace EscapeReloaded
 {
--- EscapeReloaded/EventHandlers.cs
+++ EscapeReloaded/EventHandlers.cs
@@ -1,27 +1,15 @@
-using Exiled.API.Extensions;
-using Exiled.API.Features;
 using Exiled.Events.EventArgs;
-using Exiled.Permissions.Extensions;
-using System.Linq;
-using Exiled.API.Enums;
-using Hints;
-using System.Collections.Generic;
-using UnityEngine;
-using MEC;
 
 namespace EscapeReloaded
 {
-	public class EventHandlers
-	{
+    public class EventHandlers
+    {
+        public void OnRoundStart()
+        {
+        }
 
-		public void OnRoundStart()
-		{
-
-		}
-
-		public void OnRoundEnd(RoundEndedEventArgs ev)
-		{
-
-		}
-	}
+        public void OnRoundEnd(RoundEndedEventArgs ev)
+        {
+        }
+    }
 }
